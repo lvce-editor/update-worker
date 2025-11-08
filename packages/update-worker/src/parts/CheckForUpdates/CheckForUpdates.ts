00bbb8fe7a67b88b66fe1f631d205c7f70dfbb0d
Le Vivilet <levivilet@pm.me>
2025-11-09 00:24:46 +0100

    feature: return update result

--- packages/update-worker/src/parts/CheckForUpdates/CheckForUpdates.ts
+++ packages/update-worker/src/parts/CheckForUpdates/CheckForUpdates.ts
@@ -5,28 +5,53 @@ import { getLatestReleaseVersion } from '../GetLatestReleaseVersion/GetLatestRel
 import { installAndRestart } from '../InstallAndRestart/InstallAndRestart.ts'
 import { shouldUpdate } from '../ShouldUpdate/ShouldUpdate.ts'
 
-export const checkForUpdates = async (updateSetting: string, repository: string): Promise<void> => {
-  const info = await getLatestReleaseVersion(repository)
-  if (!info || !info.version) {
-    return
+interface UpdateResult {
+  readonly updated: boolean
+  readonly error: any
+}
+
+export const checkForUpdates = async (updateSetting: string, repository: string): Promise<UpdateResult> => {
+  try {
+    const info = await getLatestReleaseVersion(repository)
+    if (!info || !info.version) {
+      return {
+        updated: false,
+        error: undefined,
+      }
+    }
+    if (!(await shouldUpdate(updateSetting, info.version))) {
+      return {
+        updated: false,
+        error: undefined,
+      }
+    }
+    // @ts-ignore
+    await RendererWorker.invoke('Layout.setUpdateState', {
+      state: 'downloading',
+      progress: 0,
+    })
+    await downloadUpdateToCache(repository, info.version)
+    const downloadPath = '' // TODO get it from cache to disk somehow
+    const messageRestart = AutoUpdaterStrings.promptRestart()
+    const shouldRestart = await RendererWorker.invoke('ConfirmPrompt.prompt', messageRestart, {
+      confirmMessage: '',
+      title: '',
+    })
+    if (!shouldRestart) {
+      return {
+        updated: false,
+        error: undefined,
+      }
+    }
+    await installAndRestart(downloadPath)
+    return {
+      updated: true,
+      error: undefined,
+    }
+  } catch (error) {
+    return {
+      updated: false,
+      error,
+    }
   }
-  if (!(await shouldUpdate(updateSetting, info.version))) {
-    return
-  }
-  // @ts-ignore
-  await RendererWorker.invoke('Layout.setUpdateState', {
-    state: 'downloading',
-    progress: 0,
-  })
-  await downloadUpdateToCache(repository, info.version)
-  const downloadPath = '' // TODO get it from cache to disk somehow
-  const messageRestart = AutoUpdaterStrings.promptRestart()
-  const shouldRestart = await RendererWorker.invoke('ConfirmPrompt.prompt', messageRestart, {
-    confirmMessage: '',
-    title: '',
-  })
-  if (!shouldRestart) {
-    return
-  }
-  await installAndRestart(downloadPath)
 }
