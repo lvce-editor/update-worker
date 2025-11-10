59fd2c9e65ad356ab21db81058849d383cd794a8
Le Vivilet <levivilet@pm.me>
2025-11-10 10:38:31 +0100

    feature: move uiStrings

--- packages/update-worker/src/parts/AutoUpdaterStrings/AutoUpdaterStrings.ts
+++ packages/update-worker/src/parts/AutoUpdaterStrings/AutoUpdaterStrings.ts
@@ -1,12 +1,5 @@
 import * as I18NString from '@lvce-editor/i18n'
-
-/**
- * @enum {string}
- */
-const UiStrings = {
-  DoYouWantToUpdate: 'Do you want to update to version {PH1}?',
-  DoYouWantToRestart: 'The Update has been downloaded. Do you want to restart now?',
-}
+import * as UiStrings from '../UiStrings/UiStrings.ts'
 
 export const promptMessage = (version: string): string => {
   return I18NString.i18nString(UiStrings.DoYouWantToUpdate, { PH1: version })
