d54978d73a2cce10b80d17d74ea177c942d56b21
Le Vivilet <levivilet@pm.me>
2025-11-10 10:36:28 +0100

    feature: implement locking for update request (#28)

--- packages/update-worker/src/parts/CheckForUpdates/CheckForUpdates.ts
+++ packages/update-worker/src/parts/CheckForUpdates/CheckForUpdates.ts
@@ -1,88 +1,20 @@
-import { RendererWorker } from '@lvce-editor/rpc-registry'
-import * as AutoUpdaterStrings from '../AutoUpdaterStrings/AutoUpdaterStrings.ts'
-import { confirmPrompt } from '../ConfirmPrompt/ConfirmPrompt.ts'
-import { downloadToDisk } from '../DownloadToDisk/DownloadToDisk.ts'
-import { downloadUpdateToCache } from '../DownloadUpdateToCache/DownloadUpdateToCache.ts'
-import { existsFile } from '../ExistsFile/ExistsFile.ts'
-import { getCache } from '../GetCache/GetCache.ts'
-import { getDiskPath } from '../GetDiskPath/GetDiskPath.ts'
-import { getLatestReleaseVersion } from '../GetLatestReleaseVersion/GetLatestReleaseVersion.ts'
-import { getUpdateUrl } from '../GetUpdateUrl/GetUpdateUrl.ts'
-import { installAndRestart } from '../InstallAndRestart/InstallAndRestart.ts'
-import { isCached } from '../IsCached/IsCached.ts'
-import { isOnline } from '../IsOnline/IsOnline.ts'
-import { shouldUpdate } from '../ShouldUpdate/ShouldUpdate.ts'
-
-export interface UpdateResult {
-  readonly updated: boolean
-  readonly error: any
-}
+import type { UpdateResult } from '../DoCheckForUpdates/DoCheckForUpdates.ts'
+import { doCheckForUpdates } from '../DoCheckForUpdates/DoCheckForUpdates.ts'
+import { requestLock } from '../RequestLock/RequestLock.ts'
 
 export const checkForUpdates = async (updateSetting: string, repository: string): Promise<UpdateResult> => {
-  try {
-    if (!isOnline()) {
+  const lockName = 'electron-updates'
+  const bucketName = 'electron-updates'
+  const cacheName = 'electron-updates'
+  const result = await requestLock(lockName, async (lock) => {
+    if (!lock) {
       return {
         updated: false,
         error: undefined,
       }
     }
-    const bucketName = 'electron-updates'
-    const cacheName = 'electron-updates'
-    const info = await getLatestReleaseVersion(repository)
-    if (!info || !info.version) {
-      return {
-        updated: false,
-        error: undefined,
-      }
-    }
-    const cache = await getCache(bucketName, cacheName)
-
-    if (!(await shouldUpdate(updateSetting, info.version))) {
-      return {
-        updated: false,
-        error: undefined,
-      }
-    }
-    const downloadUrl = getUpdateUrl(repository, info.version)
-
-    // @ts-ignore
-    await RendererWorker.invoke('Layout.setUpdateState', {
-      state: 'downloading',
-      progress: 0,
-    })
-    if (!(await isCached(downloadUrl, cache))) {
-      await downloadUpdateToCache(downloadUrl, cache)
-    }
-    const messageRestart = AutoUpdaterStrings.promptRestart()
-    const shouldRestart = await confirmPrompt(messageRestart)
-    if (!shouldRestart) {
-      return {
-        updated: false,
-        error: undefined,
-      }
-    }
-    const response = await cache.match(downloadUrl)
-    if (!response) {
-      return {
-        updated: false,
-        error: undefined,
-      }
-    }
-    const diskPath = await getDiskPath(downloadUrl)
-
-    if (!(await existsFile(diskPath))) {
-      await downloadToDisk(downloadUrl, response)
-    }
-    await installAndRestart(diskPath)
-    return {
-      updated: true,
-      error: undefined,
-    }
-  } catch (error) {
-    console.error(error)
-    return {
-      updated: false,
-      error: undefined,
-    }
-  }
+    const result = await doCheckForUpdates(updateSetting, repository, bucketName, cacheName)
+    return result
+  })
+  return result
 }
---
+++ packages/update-worker/src/parts/DoCheckForUpdates/DoCheckForUpdates.ts
@@ -0,0 +1,86 @@
+import { RendererWorker } from '@lvce-editor/rpc-registry'
+import * as AutoUpdaterStrings from '../AutoUpdaterStrings/AutoUpdaterStrings.ts'
+import { confirmPrompt } from '../ConfirmPrompt/ConfirmPrompt.ts'
+import { downloadToDisk } from '../DownloadToDisk/DownloadToDisk.ts'
+import { downloadUpdateToCache } from '../DownloadUpdateToCache/DownloadUpdateToCache.ts'
+import { existsFile } from '../ExistsFile/ExistsFile.ts'
+import { getCache } from '../GetCache/GetCache.ts'
+import { getDiskPath } from '../GetDiskPath/GetDiskPath.ts'
+import { getLatestReleaseVersion } from '../GetLatestReleaseVersion/GetLatestReleaseVersion.ts'
+import { getUpdateUrl } from '../GetUpdateUrl/GetUpdateUrl.ts'
+import { installAndRestart } from '../InstallAndRestart/InstallAndRestart.ts'
+import { isCached } from '../IsCached/IsCached.ts'
+import { isOnline } from '../IsOnline/IsOnline.ts'
+import { shouldUpdate } from '../ShouldUpdate/ShouldUpdate.ts'
+
+export interface UpdateResult {
+  readonly updated: boolean
+  readonly error: any
+}
+
+export const doCheckForUpdates = async (updateSetting: string, repository: string, bucketName: string, cacheName: string): Promise<UpdateResult> => {
+  try {
+    if (!isOnline()) {
+      return {
+        updated: false,
+        error: undefined,
+      }
+    }
+    const info = await getLatestReleaseVersion(repository)
+    if (!info || !info.version) {
+      return {
+        updated: false,
+        error: undefined,
+      }
+    }
+    const cache = await getCache(bucketName, cacheName)
+
+    if (!(await shouldUpdate(updateSetting, info.version))) {
+      return {
+        updated: false,
+        error: undefined,
+      }
+    }
+    const downloadUrl = getUpdateUrl(repository, info.version)
+
+    // @ts-ignore
+    await RendererWorker.invoke('Layout.setUpdateState', {
+      state: 'downloading',
+      progress: 0,
+    })
+    if (!(await isCached(downloadUrl, cache))) {
+      await downloadUpdateToCache(downloadUrl, cache)
+    }
+    const messageRestart = AutoUpdaterStrings.promptRestart()
+    const shouldRestart = await confirmPrompt(messageRestart)
+    if (!shouldRestart) {
+      return {
+        updated: false,
+        error: undefined,
+      }
+    }
+    const response = await cache.match(downloadUrl)
+    if (!response) {
+      return {
+        updated: false,
+        error: undefined,
+      }
+    }
+    const diskPath = await getDiskPath(downloadUrl)
+
+    if (!(await existsFile(diskPath))) {
+      await downloadToDisk(downloadUrl, response)
+    }
+    await installAndRestart(diskPath)
+    return {
+      updated: true,
+      error: undefined,
+    }
+  } catch (error) {
+    console.error(error)
+    return {
+      updated: false,
+      error: undefined,
+    }
+  }
+}
