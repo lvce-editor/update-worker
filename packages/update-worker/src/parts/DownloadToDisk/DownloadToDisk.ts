c3c11dc2ce1d2d2e3b4aec2d275d4e16dc45690d
Le Vivilet <levivilet@pm.me>
2025-11-16 20:34:01 +0100

    fix: create parent folder before writing to disk (#34)

--- packages/update-worker/src/parts/DownloadToDisk/DownloadToDisk.ts
+++ packages/update-worker/src/parts/DownloadToDisk/DownloadToDisk.ts
@@ -1,7 +1,16 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
+const getParentPath = (path: string): string => {
+  const slashIndex = path.lastIndexOf('/')
+  if (slashIndex === -1) {
+    return ''
+  }
+  return path.slice(slashIndex + 1)
+}
+
 export const downloadToDisk = async (diskPath: string, response: Response): Promise<void> => {
-  // @ts-ignore
+  const parentPath = getParentPath(diskPath)
+  await RendererWorker.invoke('FileSystem.mkdir', parentPath)
   const blob = await response.blob()
   // @ts-ignore
   await RendererWorker.invoke('FileSystem.writeBlob', diskPath, blob)
