64b89a6ad3331292c128d8357d9a159423110bbd
Le Vivilet <levivilet@pm.me>
2026-05-25 16:09:03 +0200

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,13 @@
 import * as config from '@lvce-editor/eslint-config'
 import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 
-export default [...config.default, ...actions.default]
+export default [
+  ...config.default,
+  ...actions.default,
+  {
+    rules: {
+      '@cspell/spellchecker': 'off',
+      'github-actions/action-versions': 'off',
+    },
+  },
+]
