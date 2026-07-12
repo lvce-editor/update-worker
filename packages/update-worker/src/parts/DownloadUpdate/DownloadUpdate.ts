9218e17b1e786f55aa7595fc2ea9e3af932f38bb
Le Vivilet <levivilet@pm.me>
2026-07-12 17:28:18 +0000

    fix: resolve eslint config issues

--- packages/update-worker/src/parts/DownloadUpdate/DownloadUpdate.ts
+++ packages/update-worker/src/parts/DownloadUpdate/DownloadUpdate.ts
@@ -1,7 +1,6 @@
 export const downloadUpdate = async (updateUrl: string): Promise<Response> => {
   const response = await fetch(updateUrl, {
     priority: 'low',
-    redirect: 'follow',
   })
   if (!response.ok) {
     throw new Error(response.statusText)
