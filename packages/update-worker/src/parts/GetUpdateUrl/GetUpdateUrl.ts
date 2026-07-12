63219190aab30e89b3437c076625e17fb2b5c604
Le Vivilet <levivilet@pm.me>
2026-07-12 19:31:04 +0200

    chore: update eslint and eslint config to latest (#60)

--- packages/update-worker/src/parts/GetUpdateUrl/GetUpdateUrl.ts
+++ packages/update-worker/src/parts/GetUpdateUrl/GetUpdateUrl.ts
@@ -1,5 +1,5 @@
 export const getUpdateUrl = (repository: string, fileNameTemplate: string, version: string): string => {
-  const fileName = fileNameTemplate.replace('${version}', version)
+  const fileName = fileNameTemplate.replace('${version}', () => version)
   const url = `https://github.com/${repository}/releases/download/v${version}/${fileName}`
   return url
 }
