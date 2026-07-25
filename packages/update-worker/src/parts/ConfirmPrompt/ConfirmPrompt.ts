e9c4ba14775a7e55b721195b215302e6fa3321e1
Le Vivilet <levivilet@pm.me>
2026-07-25 21:14:37 +0000

    feature: use dialog worker directly

--- packages/update-worker/src/parts/ConfirmPrompt/ConfirmPrompt.ts
+++ packages/update-worker/src/parts/ConfirmPrompt/ConfirmPrompt.ts
@@ -1,8 +1,8 @@
 import { PlatformType } from '@lvce-editor/constants'
-import { RendererWorker } from '@lvce-editor/rpc-registry'
+import { DialogWorker } from '@lvce-editor/rpc-registry'
 
 export const confirmPrompt = async (message: string): Promise<boolean> => {
-  const result = await RendererWorker.invoke('ConfirmPrompt.prompt', message, {
+  const result = await DialogWorker.invoke('ConfirmPrompt.prompt', message, {
     // @ts-ignore
     platform: PlatformType.Electron,
   })
