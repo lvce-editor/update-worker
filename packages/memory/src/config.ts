664ae7fb6150295334c0d4b5ba7819632563b425
Le Vivilet <levivilet@pm.me>
2026-07-26 15:28:21 +0000

    feature: use npm workspaces

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -9,4 +9,4 @@ export const instantiationsPath = join(root, 'packages', 'update-worker')
 
 export const workerPath = join(root, '.tmp/dist/dist/updateWorkerMain.js')
 
-export const playwrightPath = import.meta.resolve('../../e2e/node_modules/playwright/index.mjs').toString()
+export const playwrightPath = import.meta.resolve('../../../node_modules/playwright/index.mjs').toString()
--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -16,22 +16,20 @@ fi
 
 function updateDependencies {
   echo "updating dependencies..."
-  OUTPUT=`ncu -u -x @types/node -x lerna -x typescript`
+  OUTPUT=`ncu -u -x @types/node -x typescript`
   SUB='All dependencies match the latest package versions'
   if [[ "$OUTPUT" == *"$SUB"* ]]; then
     echo "$OUTPUT"
-  else
-    rm -rf node_modules package-lock.json dist
-    npm install
   fi
 }
 
-                                                       updateDependencies             &&
-cd packages/build                                   && updateDependencies && cd ../.. &&
-cd packages/e2e                                     && updateDependencies && cd ../.. &&
-cd packages/memory                                  && updateDependencies && cd ../.. &&
-cd packages/server                                  && updateDependencies && cd ../.. &&
-cd packages/update-worker                        && updateDependencies && cd ../.. &&
+updateDependencies &&
+for package in packages/*; do
+  cd "$package" || exit
+  updateDependencies
+  cd ../.. || exit
+done &&
+npm install &&
 
 echo "Great Success!"
 
